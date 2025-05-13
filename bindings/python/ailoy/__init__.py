e125a08141c86643ac842399d1cbeadc3201a363
jhlee525 <bmrcreative90@gmail.com>
2025-05-13 15:28:36 +0900

    API name & package name changed

--- bindings/python/ailoy/__init__.py
+++ bindings/python/ailoy/__init__.py
@@ -1,2 +1,3 @@
-from .reflective_executor import ReflectiveExecutor  # noqa: F401
+from .assistant import Assistant  # noqa: F401
 from .runtime import AsyncRuntime, Runtime  # noqa: F401
+from .vector_store import VectorStore  # noqa: F401
