530071ebf493900e3990d0f74d39797bcfc12fb0
khj809 <onsealeatang@gmail.com>
2025-05-14 20:42:25 +0900

    change vectorstore runtime from async to sync

--- bindings/python/tests/test_1_runtime_async.py
+++ bindings/python/tests/test_1_runtime_async.py
@@ -3,13 +3,12 @@ import time
 import pytest
 
 from ailoy.runtime import AsyncRuntime
-from ailoy.vector_store import FAISSConfig, VectorStore
 
 pytestmark = [pytest.mark.runtime, pytest.mark.asyncio]
 
 
 @pytest.fixture(scope="module")
-def async_runtime():
+def runtime():
     print("creating async runtime")
     time.sleep(3)
     rt = AsyncRuntime("inproc://async")
@@ -17,37 +16,21 @@ def async_runtime():
     rt.close()
 
 
-async def test_async_echo(async_runtime: AsyncRuntime):
-    await async_runtime.call("echo", "hello world") == "hello world"
+async def test_async_echo(runtime: AsyncRuntime):
+    await runtime.call("echo", "hello world") == "hello world"
 
 
-async def test_async_spell(async_runtime: AsyncRuntime):
+async def test_async_spell(runtime: AsyncRuntime):
     i = 0
-    async for out in async_runtime.call_iter("spell", "abcdefghijk"):
+    async for out in runtime.call_iter("spell", "abcdefghijk"):
         assert out == "abcdefghijk"[i]
         i += 1
 
 
-async def test_vectorstore(async_runtime: AsyncRuntime):
-    vs = VectorStore(async_runtime, FAISSConfig())
-    await vs.initialize()
-
-    doc1 = "BGE M3 is an embedding model supporting dense retrieval, lexical matching and multi-vector interaction."
-    doc2 = "BM25 is a bag-of-words retrieval function that ranks a set of documents based on the query terms appearing in each document"  # noqa: E501
-    await vs.insert(document=doc1, metadata={"value": "BGE-M3"})
-    await vs.insert(document=doc2, metadata={"value": "BM25"})
-
-    resp = await vs.retrieve(query="What is BGE M3?", top_k=1)
-    assert len(resp) == 1
-    assert resp[0].metadata["value"] == "BGE-M3"
-
-    await vs.deinitialize()
-
-
-async def test_async_infer_language_model(async_runtime: AsyncRuntime):
-    await async_runtime.define("tvm_language_model", "lm0", {"model": "Qwen/Qwen3-0.6B"})
+async def test_async_infer_language_model(runtime: AsyncRuntime):
+    await runtime.define("tvm_language_model", "lm0", {"model": "Qwen/Qwen3-0.6B"})
     input = {"messages": [{"role": "user", "content": "Who are you?"}]}
     print("\n")
-    async for out in async_runtime.call_iter_method("lm0", "infer", input):
+    async for out in runtime.call_iter_method("lm0", "infer", input):
         print(out["message"]["content"], end="", flush=True)
-    await async_runtime.delete("lm0")
+    await runtime.delete("lm0")
