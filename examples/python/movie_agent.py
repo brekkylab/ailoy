e4a424462aa7208f64dff9a6bf0846670a4a5a7a
Park Woorak <grf0503@gmail.com>
2025-05-22 23:44:46 +0900

    fix: unexpected change

--- examples/python/movie_agent.py
+++ examples/python/movie_agent.py
@@ -12,7 +12,7 @@ async def main():
     if tmdb_api_key is None:
         tmdb_api_key = input("Enter TMDB API Key: ")
 
-    agent = Agent(rt, model_name="Qwen/Qwen3-4B")
+    agent = Agent(rt, model_name="Qwen/Qwen3-8B")
 
     agent.add_tools_from_preset("tmdb", authenticator=BearerAuthenticator(tmdb_api_key))
 
