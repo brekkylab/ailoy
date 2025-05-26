2bf282d84061684425201319c87d743da47eda17
khj809 <onsealeatang@gmail.com>
2025-05-26 13:50:46 +0900

    fix

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "BrekkyLab", // Usually your GitHub org/user name.
+  organizationName: "brekkylab", // Usually your GitHub org/user name.
   projectName: "ailoy", // Usually your repo name.
 
   onBrokenLinks: "throw",
