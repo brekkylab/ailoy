ce1fc774c707548a4f1984a27293cd68469fa9ff
grf53 <grf0503@gmail.com>
2025-05-30 18:31:33 +0900

    fix: append missed assistant message in `Agent` messages

--- bindings/python/ailoy/agent.py
+++ bindings/python/ailoy/agent.py
@@ -429,6 +429,21 @@ class Agent:
             self._messages = []
         self._component_state.valid = False
 
+    def _append_assistant_text_message(self, reasoning_message: str, output_text_message: str):
+        # if output_text is empty, do nothing
+        if output_text_message.strip() == "":
+            return
+
+        # construct assistant message content considering reasoning message
+        assistant_message_content = output_text_message.strip()
+        if reasoning_message.strip() != "":
+            assistant_message_content = (
+                f"<think>\n{reasoning_message.strip()}\n</think>\n\n" + assistant_message_content
+            )
+
+        # append constructed message content into messages
+        self._messages.append(AIOutputTextMessage(role="assistant", content=assistant_message_content))
+
     def query(
         self,
         message: str,
@@ -455,11 +470,20 @@ class Agent:
             if ignore_reasoning_messages:
                 infer_args["ignore_reasoning_messages"] = ignore_reasoning_messages
 
+            reasoning_message = ""
+            output_text_message = ""
             for resp in self._runtime.call_iter_method(self._component_state.name, "infer", infer_args):
                 delta = MessageDelta.model_validate(resp)
 
                 if delta.finish_reason is None:
                     output_msg = AIOutputTextMessage.model_validate(delta.message)
+
+                    # accumulate text messages to push in batch
+                    if output_msg.reasoning:
+                        reasoning_message += output_msg.content
+                    else:
+                        output_text_message += output_msg.content
+
                     yield AgentResponseOutputText(
                         type="reasoning" if output_msg.reasoning else "output_text",
                         end_of_turn=False,
@@ -469,6 +493,10 @@ class Agent:
                     continue
 
                 if delta.finish_reason == "tool_calls":
+                    self._append_assistant_text_message(reasoning_message, output_text_message)
+                    reasoning_message = ""
+                    output_text_message = ""
+
                     tool_call_message = AIToolCallMessage.model_validate(delta.message)
                     self._messages.append(tool_call_message)
 
@@ -513,6 +541,13 @@ class Agent:
 
                 if delta.finish_reason in ["stop", "length", "error"]:
                     output_msg = AIOutputTextMessage.model_validate(delta.message)
+
+                    output_text_message += output_msg.content
+
+                    self._append_assistant_text_message(reasoning_message, output_text_message)
+                    reasoning_message = ""
+                    output_text_message = ""
+
                     yield AgentResponseOutputText(
                         type="reasoning" if output_msg.reasoning else "output_text",
                         end_of_turn=True,
