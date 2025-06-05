f97ee3844b5e16dce85e066c9e37370fc7268696
grf53 <grf0503@gmail.com>
2025-06-05 16:28:42 +0900

    docs: add logo & favicon & README.md for Python API Reference

--- bindings/python/ailoy/__init__.py
+++ bindings/python/ailoy/__init__.py
@@ -1,3 +1,6 @@
+with open("README.md", "r") as f:
+    __doc__ = f.read()
+
 from .agent import Agent  # noqa: F401
 from .runtime import AsyncRuntime, Runtime  # noqa: F401
 from .vector_store import VectorStore  # noqa: F401
