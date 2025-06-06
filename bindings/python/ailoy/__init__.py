cacdb6e1d5cd9470fd34066a053871ce66fbdef0
grf53 <grf0503@gmail.com>
2025-06-06 16:20:56 +0900

    fix: consider various environment to set root module docstring

--- bindings/python/ailoy/__init__.py
+++ bindings/python/ailoy/__init__.py
@@ -1,5 +1,23 @@
-with open("README.md", "r") as f:
-    __doc__ = f.read()
+if __doc__ is None:
+    try:
+        import importlib.metadata
+
+        meta = importlib.metadata.metadata("ailoy-py")
+        __doc__ = meta.get("Description")
+    except importlib.metadata.PackageNotFoundError:
+        pass
+
+if __doc__ is None:
+    import os.path
+
+    if os.path.isfile("README.md"):
+        with open("README.md", "r") as f:
+            __doc__ = f.read()
+    elif os.path.isfile("../README.md"):
+        with open("../README.md", "r") as f:
+            __doc__ = f.read()
+    else:
+        __doc__ = "# ailoy-py\n\nPython binding for Ailoy runtime APIs"
 
 from .agent import Agent  # noqa: F401
 from .runtime import AsyncRuntime, Runtime  # noqa: F401
