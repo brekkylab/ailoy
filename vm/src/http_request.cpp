80df801241be2e4f8978c5b1e877c031e8638a84
khj809 <onsealeatang@gmail.com>
2025-07-15 16:47:35 +0900

    apply ailoy::http to http_request op

--- vm/src/http_request.cpp
+++ vm/src/http_request.cpp
@@ -1,88 +1,11 @@
 #include "http_request.hpp"
 
-#include <httplib.h>
+#include <magic_enum/magic_enum.hpp>
 
 #include "exception.hpp"
+#include "http.hpp"
 
 namespace ailoy {
-
-http_response_t run_http_request(const http_request_t &req) {
-  auto url = req.url;
-  auto method = req.method;
-  auto headers = req.headers;
-  auto body = req.body;
-
-  std::string host, path;
-  size_t pos = url.find("://");
-  if (pos != std::string::npos) {
-    pos += 3;
-    size_t slash_pos = url.find("/", pos);
-    if (slash_pos == std::string::npos) {
-      host = url.substr(pos);
-      path = "/";
-    } else {
-      host = url.substr(pos, slash_pos - pos);
-      path = url.substr(slash_pos);
-    }
-  } else {
-    return http_response_t{
-        .status_code = 400,
-        .headers = {},
-        .body = "Invalid URL format",
-    };
-  }
-
-  httplib::Client client(host);
-  client.set_follow_location(true);
-  client.set_read_timeout(5, 0);
-  client.set_connection_timeout(5, 0);
-
-  httplib::Headers httplib_headers;
-  for (const auto &[key, value] : req.headers) {
-    httplib_headers.emplace(key, value);
-  }
-
-  http_response_t response;
-  httplib::Result result;
-  std::string body_str = body.has_value() ? body->data() : "";
-
-  std::string content_type;
-  if (req.headers.contains("Content-Type")) {
-    content_type = req.headers.find("Content-Type")->second;
-  } else {
-    content_type = "text/plain";
-  }
-
-  if (req.method == "GET") {
-    result = client.Get(path, httplib_headers);
-  } else if (req.method == "POST") {
-    result = client.Post(path, httplib_headers, body_str, content_type);
-  } else if (req.method == "PUT") {
-    result = client.Put(path, httplib_headers, body_str, content_type);
-  } else if (req.method == "DELETE") {
-    result = client.Delete(path, httplib_headers);
-  } else {
-    return http_response_t{
-        .status_code = 400,
-        .headers = {},
-        .body = "Unsupported HTTP method",
-    };
-  }
-
-  if (result) {
-    response.status_code = result->status;
-    response.body = result->body;
-
-    for (const auto &[key, value] : result->headers) {
-      response.headers[key] = value;
-    }
-  } else {
-    response.status_code = 500;
-    response.body = "Request Failed";
-  }
-
-  return response;
-}
 
 value_or_error_t http_request_op(std::shared_ptr<const value_t> inputs) {
   if (!inputs->is_type_of<map_t>())
@@ -104,9 +27,10 @@ value_or_error_t http_request_op(std::shared_ptr<const value_t> inputs) {
                                      input_map->at("method")->get_type()));
   auto method = input_map->at<string_t>("method");
   if (!(*method == "GET" || *method == "POST" || *method == "PUT" ||
-        *method == "DELETE")) {
+        *method == "PATCH" || *method == "DELETE")) {
     return error_output_t(value_error("HTTP Request", "method",
-                                      "GET | POST | PUT | DELETE", *method));
+                                      "GET | POST | PUT | PATCH | DELETE",
+                                      *method));
   }
   std::shared_ptr<const map_t> headers = create<map_t>();
   if (input_map->contains("headers")) {
@@ -123,30 +47,33 @@ value_or_error_t http_request_op(std::shared_ptr<const value_t> inputs) {
     body = input_map->at<string_t>("body");
   }
 
-  std::unordered_map<std::string, std::string> req_headers_map;
+  ailoy::http::headers_t req_headers;
   for (auto it = headers->begin(); it != headers->end(); it++) {
     std::string key = it->first;
     std::string value = *it->second->as<string_t>();
-    req_headers_map.emplace(key, value);
+    req_headers.emplace(key, value);
   }
 
-  ailoy::http_request_t req = {
+  auto resp = ailoy::http::request({
       .url = *url,
-      .method = *method,
-      .headers = req_headers_map,
+      .method = magic_enum::enum_cast<ailoy::http::method_t>(*method).value(),
+      .headers = req_headers,
       .body = body->data(),
-  };
-  auto resp = ailoy::run_http_request(req);
+  });
 
-  auto resp_headers_map = create<map_t>();
-  for (const auto &[key, value] : resp.headers) {
-    resp_headers_map->insert_or_assign(key, create<string_t>(value));
+  if (!resp) {
+    return error_output_t(resp.error());
+  }
+
+  auto resp_headers = create<map_t>();
+  for (const auto &[key, value] : resp->headers) {
+    resp_headers->insert_or_assign(key, create<string_t>(value));
   }
 
   auto outputs = create<map_t>();
-  outputs->insert_or_assign("status_code", create<uint_t>(resp.status_code));
-  outputs->insert_or_assign("headers", resp_headers_map);
-  outputs->insert_or_assign("body", create<bytes_t>(resp.body));
+  outputs->insert_or_assign("status_code", create<uint_t>(resp->status_code));
+  outputs->insert_or_assign("headers", resp_headers);
+  outputs->insert_or_assign("body", create<bytes_t>(resp->body));
   return outputs;
 }
 
--- vm/src/http_request.hpp
+++ vm/src/http_request.hpp
@@ -1,27 +1,9 @@
 #pragma once
 
-#include <optional>
-#include <unordered_map>
-
 #include "module.hpp"
 #include "value.hpp"
 
 namespace ailoy {
-
-struct http_request_t {
-  std::string url;
-  std::string method;
-  std::unordered_map<std::string, std::string> headers;
-  std::optional<std::string> body = std::nullopt;
-};
-
-struct http_response_t {
-  int status_code;
-  std::unordered_map<std::string, std::string> headers;
-  std::string body;
-};
-
-http_response_t run_http_request(const http_request_t &req);
 
 value_or_error_t http_request_op(std::shared_ptr<const value_t> inputs);
 
