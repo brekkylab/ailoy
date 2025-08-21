35489bfa2bdb2e7ab4f2397f1b68568b41e82fc4
khj809 <onsealeatang@gmail.com>
2025-08-21 22:31:54 +0900

    enable gemini

--- v2/src/tool/mcp/native.rs
+++ v2/src/tool/mcp/native.rs
@@ -158,7 +158,8 @@ mod tests {
         let part = parts[0].clone();
         assert_eq!(part.is_text(), true);
 
-        let parsed_part: serde_json::Value = serde_json::from_str(&part.as_str().unwrap()).unwrap();
+        let parsed_part: serde_json::Value =
+            serde_json::from_str(&part.to_string().unwrap()).unwrap();
         assert_eq!(parsed_part["timezone"].as_str(), Some("Asia/Seoul"));
         assert_eq!(parsed_part["is_dst"].as_bool(), Some(false));
         assert_eq!(
