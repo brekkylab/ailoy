6c0a3019f8cd2bb1e11034b7ebad208d47024fc4
Park Woorak <grf0503@gmail.com>
2025-08-26 15:22:39 +0900

    Vectorstore for V2 (#172)

---
+++ v2/src/knowledge_base/vector_store.rs
@@ -0,0 +1,55 @@
+use crate::async_trait;
+use anyhow::Result;
+use serde_json::{Map, Value as Json};
+
+use crate::utils::{MaybeSend, MaybeSync};
+
+pub type Embedding = Vec<f32>;
+pub type Metadata = Map<String, Json>;
+
+#[derive(Debug)]
+pub struct AddInput {
+    pub embedding: Embedding,
+    pub document: String,
+    pub metadata: Option<Metadata>,
+}
+
+#[derive(Debug)]
+pub struct GetResult {
+    pub id: String,
+    pub document: String,
+    pub metadata: Option<Metadata>,
+    pub embedding: Embedding,
+}
+
+#[derive(Debug)]
+pub struct RetrieveResult {
+    pub id: String,
+    pub document: String,
+    pub metadata: Option<Metadata>,
+    pub distance: f32,
+}
+
+async_trait! {
+    pub trait VectorStore: MaybeSend + MaybeSync {
+        async fn add_vector(&mut self, input: AddInput) -> Result<String>;
+        async fn add_vectors(&mut self, inputs: Vec<AddInput>) -> Result<Vec<String>>;
+        async fn get_by_id(&self, id: &str) -> Result<Option<GetResult>>;
+        async fn get_by_ids(&self, ids: &[&str]) -> Result<Vec<GetResult>>;
+        async fn retrieve(
+            &self,
+            query_embedding: Embedding,
+            top_k: usize,
+        ) -> Result<Vec<RetrieveResult>>;
+        async fn batch_retrieve(
+            &self,
+            query_embeddings: Vec<Embedding>,
+            top_k: usize,
+        ) -> Result<Vec<Vec<RetrieveResult>>>;
+        async fn remove_vector(&mut self, id: &str) -> Result<()>;
+        async fn remove_vectors(&mut self, ids: &[&str]) -> Result<()>;
+        async fn clear(&mut self) -> Result<()>;
+
+        async fn count(&self) -> Result<usize>;
+    }
+}
