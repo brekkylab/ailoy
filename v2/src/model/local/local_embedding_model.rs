8c56386885e484e1b1f981cf4256b0cfd1763126
grf53 <grf0503@gmail.com>
2025-08-28 23:12:06 +0900

    test: add module test for `LocalEmbeddingModel`

--- v2/src/model/local/local_embedding_model.rs
+++ v2/src/model/local/local_embedding_model.rs
@@ -9,7 +9,7 @@ use crate::{
         EmbeddingModel,
         local::{EmbeddingModelInferencer, Tokenizer},
     },
-    utils::{BoxFuture, Mutex},
+    utils::{BoxFuture, Mutex, Normalize},
 };
 use ailoy_macros::multi_platform_async_trait;
 
@@ -63,5 +63,88 @@ impl TryFromCache for LocalEmbeddingModel {
             tokenizer,
             inferencer: Mutex::new(inferencer),
         })
+    }
+}
+
+#[cfg(any(target_family = "unix", target_family = "windows"))]
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use futures::StreamExt;
+
+    fn dot(a: &Vec<f32>, b: &Vec<f32>) -> f32 {
+        if a.len() != b.len() {
+            panic!("Cannot dot two vectors of different lengths");
+        }
+
+        a.iter().zip(b.iter()).map(|(x, y)| x * y).sum()
+    }
+
+    #[tokio::test]
+    async fn infer_embedding() {
+        let cache = crate::cache::Cache::new();
+        let key = "BAAI/bge-m3";
+
+        let mut model_strm = Box::pin(cache.try_create::<LocalEmbeddingModel>(key));
+        let mut model: Option<LocalEmbeddingModel> = None;
+        while let Some(progress) = model_strm.next().await {
+            let mut progress = progress.unwrap();
+            println!(
+                "{} ({} / {})",
+                progress.comment, progress.current_task, progress.total_task
+            );
+            if progress.current_task == progress.total_task {
+                model = progress.result.take();
+            }
+        }
+        let model = Arc::new(model.unwrap());
+
+        let embedding = model.run("What is BGE M3?".to_owned()).await.unwrap();
+        assert_eq!(embedding.len(), 1024);
+    }
+
+    #[tokio::test]
+    async fn test_similariry() {
+        use futures::StreamExt;
+
+        use super::*;
+
+        let cache = crate::cache::Cache::new();
+        let key = "BAAI/bge-m3";
+
+        let mut model_strm = Box::pin(cache.try_create::<LocalEmbeddingModel>(key));
+        let mut model: Option<LocalEmbeddingModel> = None;
+        while let Some(progress) = model_strm.next().await {
+            let mut progress = progress.unwrap();
+            println!(
+                "{} ({} / {})",
+                progress.comment, progress.current_task, progress.total_task
+            );
+            if progress.current_task == progress.total_task {
+                model = progress.result.take();
+            }
+        }
+        let model = Arc::new(model.unwrap());
+
+        let query_embedding1 = model
+            .clone()
+            .run("What is BGE M3?".to_owned())
+            .await
+            .unwrap()
+            .normalized();
+        let query_embedding2 = model
+            .clone()
+            .run("Defination of BM25".to_owned())
+            .await
+            .unwrap()
+            .normalized();
+        let answer_embedding1 = model.clone().run("BGE M3 is an embedding model supporting dense retrieval, lexical matching and multi-vector interaction.".to_owned()).await.unwrap().normalized();
+        let answer_embedding2 = model.clone().run("BM25 is a bag-of-words retrieval function that ranks a set of documents based on the query terms appearing in each document".to_owned()).await.unwrap().normalized();
+        assert!(
+            dot(&query_embedding1, &answer_embedding1) > dot(&query_embedding1, &answer_embedding2)
+        );
+        assert!(
+            dot(&query_embedding2, &answer_embedding1) < dot(&query_embedding2, &answer_embedding2)
+        );
     }
 }
