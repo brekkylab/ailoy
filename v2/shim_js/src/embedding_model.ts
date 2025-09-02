67d68f864f46753547fd6b2cf3b79805a14f29e5
grf53 <grf0503@gmail.com>
2025-09-03 00:14:41 +0900

    fix: split common parts into `tvm_runtime.ts`

--- v2/shim_js/src/embedding_model.ts
+++ v2/shim_js/src/embedding_model.ts
@@ -1,81 +1,19 @@
-import { FeatureSupportError, WebGPUNotAvailableError } from "./error";
-import {
-  EmbeddingChunkingUnsupportedError,
-  EmbeddingExceedContextWindowSizeError,
-  EmbeddingInputEmptyError,
-  EmbeddingSlidingWindowError,
-  MinValueError,
-} from "./error";
-import {
-  createPolyfillWASI,
-  detectGPUDevice,
-  DLDevice,
-  Instance,
-  instantiate,
-  NDArray,
-  PackedFunc,
-  Scalar,
-  TVMObject,
-  VirtualMachine,
-} from "./tvmjs";
-
-export async function getGPUDevice(
-  requiredFeatures: GPUFeatureName[] = ["shader-f16"]
-) {
-  const gpuDetectOutput = await detectGPUDevice();
-  if (gpuDetectOutput == undefined) {
-    throw new WebGPUNotAvailableError();
-  }
-  for (const feature of requiredFeatures) {
-    if (!gpuDetectOutput.device.features.has(feature)) {
-      throw new FeatureSupportError(feature);
-    }
-  }
-  return gpuDetectOutput.device;
-}
-
-export interface NDArrayCacheEntry {
-  name: string;
-  shape: Array<number>;
-  dtype: string;
-  format: "f32-to-bf16" | "raw";
-  byteOffset: number;
-  nbytes: number;
-}
-
-export interface NDArrayShardEntry {
-  dataPath: string;
-  format: "raw-shard";
-  nbytes: number;
-  records: Array<NDArrayCacheEntry>;
-}
-
-const PAGE_SIZE = 16;
+import { EmbeddingExceedContextWindowSizeError } from "./error";
+import { Instance, PackedFunc } from "./tvmjs";
+import { init as init_tvm_runtime, TVMRuntime } from "./tvm_runtime";
 
 export class EmbeddingModel {
+  private rt: TVMRuntime;
   private tvm: Instance;
-  private vm: VirtualMachine;
-  private params: TVMObject;
   private contextWindowSize: number;
 
   private fPrefill: PackedFunc;
-  private device: DLDevice;
 
-  constructor(
-    tvm: Instance,
-    device: DLDevice,
-    vm: VirtualMachine,
-    params: TVMObject,
-    contextWindowSize: number
-  ) {
-    this.tvm = tvm;
-    this.vm = vm;
-    this.params = params;
-
-    this.fPrefill = tvm.detachFromCurrentScope(vm.getFunction("prefill"));
-    this.contextWindowSize = contextWindowSize;
-
-    this.device = device;
+  constructor(rt: TVMRuntime) {
+    this.rt = rt;
+    this.tvm = this.rt.tvm;
+    this.contextWindowSize = this.rt.metadata.context_window_size || 8192;
+    this.fPrefill = this.rt.get_function("prefill");
   }
 
   async infer(tokens: Uint32Array): Promise<Float32Array | Uint16Array> {
@@ -103,20 +41,20 @@ export class EmbeddingModel {
     const mask_cpu = Array(inputSize).fill(1);
     const input = this.tvm.withNewScope(() => {
       return this.tvm.detachFromCurrentScope(
-        this.tvm.empty([1, inputSize], "int32", this.device)
+        this.tvm.empty([1, inputSize], "int32", this.rt.device)
       );
     });
     input.copyFrom(input_cpu);
     const mask = this.tvm.withNewScope(() => {
       return this.tvm.detachFromCurrentScope(
-        this.tvm.empty([1, inputSize], "int32", this.device)
+        this.tvm.empty([1, inputSize], "int32", this.rt.device)
       );
     });
     mask.copyFrom(mask_cpu);
-    await this.device.sync();
+    await this.rt.device.sync();
 
-    const logitsCurBatchOnGPU = this.fPrefill(input, mask, this.params);
-    await this.device.sync();
+    const logitsCurBatchOnGPU = this.fPrefill(input, mask, this.rt.params);
+    await this.rt.device.sync();
 
     const hidden_size = logitsCurBatchOnGPU.shape[2];
     let logitsCurBatchOnCPU = this.tvm.empty(
@@ -126,7 +64,7 @@ export class EmbeddingModel {
     );
     logitsCurBatchOnCPU.copyFrom(logitsCurBatchOnGPU);
     logitsCurBatchOnCPU = logitsCurBatchOnCPU.view([inputSize * hidden_size]);
-    await this.device.sync();
+    await this.rt.device.sync();
 
     let logitsCurBatchOnCPUArray: Float32Array | Uint16Array;
     if (logitsCurBatchOnCPU.dtype === "float16") {
@@ -144,58 +82,7 @@ export class EmbeddingModel {
 export async function init(
   cache_contents: Record<string, ArrayBuffer>
 ): Promise<EmbeddingModel> {
-  // Load WASM
-  const tvmBytes = cache_contents["rt.wasm"];
-  delete cache_contents["rt.wasm"];
-  const tvm = await instantiate(tvmBytes, createPolyfillWASI(), undefined);
-
-  // Initialize GPU
-  const gpu: GPUDevice = await getGPUDevice();
-  tvm.initWebGPU(gpu);
-  const device = tvm.webgpu(0);
-
-  tvm.beginScope();
-
-  // Load ndarray cache
-  const ndarray_cache = JSON.parse(
-    new TextDecoder().decode(cache_contents["ndarray-cache.json"])
-  );
-  delete cache_contents["ndarray-cache.json"];
-  const entries: Array<NDArrayShardEntry> = ndarray_cache.records;
-
-  // Register parameters
-  for (const entry of entries) {
-    const buffer = cache_contents[entry.dataPath];
-    delete cache_contents[entry.dataPath];
-    for (const record of entry.records) {
-      const bufferPart = buffer.slice(
-        record.byteOffset,
-        record.byteOffset + record.nbytes
-      );
-      await tvm.ndarrayCacheUpdateBuffer(device, record, bufferPart);
-    }
-  }
-
-  // Load VM
-  const vm = tvm.detachFromCurrentScope(tvm.createVirtualMachine(device));
-
-  // Load parameters
-  const fgetMetadata = vm.getFunction("_metadata");
-  const metadataStr = fgetMetadata().toString();
-  const metadata = JSON.parse(metadataStr);
-
-  const contextWindowSize = metadata.context_window_size || 8192;
-
-  const paramNames: string[] = [];
-  metadata.params.forEach((param: any) => {
-    paramNames.push(param.name);
-  });
-  const params = tvm.detachFromCurrentScope(
-    tvm.getParamsFromCacheByName(paramNames)
-  );
-
-  // Return
-  const rv = new EmbeddingModel(tvm, device, vm, params, contextWindowSize);
-  tvm.endScope();
+  const rt = await init_tvm_runtime(cache_contents);
+  const rv = new EmbeddingModel(rt);
   return rv;
 }
