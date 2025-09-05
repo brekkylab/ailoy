9e2e2af6b80d9c8b292ee30089f1bd42f5a4b9d1
grf53 <grf0503@gmail.com>
2025-09-05 15:37:35 +0900

    feat: add log macros can be used like `print!`

--- v2/src/utils/log.rs
+++ v2/src/utils/log.rs
@@ -12,12 +12,26 @@ pub fn debug(s: impl Into<String> + std::fmt::Display) {
     web_sys::console::debug_1(&s.to_string().into());
 }
 
+#[macro_export]
+macro_rules! debug_fmt {
+    ($($args:tt)*) => {
+        $crate::utils::log::debug(format!($($args)*))
+    };
+}
+
 pub fn info(s: impl Into<String> + std::fmt::Display) {
     #[cfg(not(target_arch = "wasm32"))]
     log::info!("{}", s);
 
     #[cfg(target_arch = "wasm32")]
     web_sys::console::info_1(&s.to_string().into());
+}
+
+#[macro_export]
+macro_rules! info_fmt {
+    ($($args:tt)*) => {
+        $crate::utils::log::info(format!($($args)*))
+    };
 }
 
 pub fn warn(s: impl Into<String> + std::fmt::Display) {
@@ -28,10 +42,24 @@ pub fn warn(s: impl Into<String> + std::fmt::Display) {
     web_sys::console::warn_1(&s.to_string().into());
 }
 
+#[macro_export]
+macro_rules! warn_fmt {
+    ($($args:tt)*) => {
+        $crate::utils::log::warn(format!($($args)*))
+    };
+}
+
 pub fn error(s: impl Into<String> + std::fmt::Display) {
     #[cfg(not(target_arch = "wasm32"))]
     log::error!("{}", s);
 
     #[cfg(target_arch = "wasm32")]
     web_sys::console::error_1(&s.to_string().into());
+}
+
+#[macro_export]
+macro_rules! error_fmt {
+    ($($args:tt)*) => {
+        $crate::utils::log::error(format!($($args)*))
+    };
 }
