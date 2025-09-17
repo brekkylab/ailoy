1a9ececc072b38255f2bb302248aaa28a0aa9b37
jhlee525 <bmrcreative90@gmail.com>
2025-09-17 23:13:54 +0900

    update marshal

--- v2/src/value/marshal.rs
+++ v2/src/value/marshal.rs
@@ -1,3 +1,5 @@
+use serde::{Deserialize, Serialize};
+
 use crate::value::Value;
 
 pub trait Marshal<T>: Default {
@@ -5,66 +7,49 @@ pub trait Marshal<T>: Default {
 }
 
 pub trait Unmarshal<T>: Default {
-    fn unmarshal(&mut self, val: Value) -> T;
+    fn unmarshal(&mut self, val: Value) -> Result<T, String>;
 }
 
-// pub trait EncodeValue<T>: Default {
-//     fn encode(&mut self, item: &T) -> Value;
-// }
+pub struct Marshaled<'d, D, M: Marshal<D>> {
+    data: &'d D,
+    m: std::marker::PhantomData<M>,
+}
 
-// pub trait DecodeValue<T>: Default {
-//     fn decode(&mut self, prim: &Value) -> Result<T, ValueError>;
-// }
+impl<'d, D, M: Marshal<D>> Serialize for Marshaled<'d, D, M> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut m = M::default();
+        let v = m.marshal(self.data);
+        v.serialize(serializer)
+    }
+}
 
-// pub struct Encodable<'data, Data, Encoder: EncodeValue<Data>> {
-//     data: &'data Data,
-//     encoder: std::marker::PhantomData<Encoder>,
-// }
+pub struct Unmarshaled<D, U: Unmarshal<D>> {
+    data: D,
+    u: std::marker::PhantomData<U>,
+}
 
-// impl<'data, Data, Encoder: EncodeValue<Data>> Encodable<'data, Data, Encoder> {
-//     pub fn new(data: &'data Data) -> Self {
-//         Self {
-//             data,
-//             encoder: std::marker::PhantomData::default(),
-//         }
-//     }
-// }
+impl<D, U: Unmarshal<D>> Unmarshaled<D, U> {
+    pub fn get(self) -> D {
+        self.data
+    }
+}
 
-// impl<'data, Data, Encoder: EncodeValue<Data>> Serialize for Encodable<'data, Data, Encoder> {
-//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-//     where
-//         S: serde::Serializer,
-//     {
-//         let mut encoder = Encoder::default();
-//         let prim = encoder.encode(self.data);
-//         prim.serialize(serializer)
-//     }
-// }
-
-// pub struct Decodable<Data, Decoder: DecodeValue<Data>> {
-//     data: Data,
-//     decoder: std::marker::PhantomData<Decoder>,
-// }
-
-// impl<Data, Decoder: DecodeValue<Data>> Decodable<Data, Decoder> {
-//     pub fn to_data(self) -> Data {
-//         self.data
-//     }
-// }
-
-// impl<'de, Data, Decoder: DecodeValue<Data>> Deserialize<'de> for Decodable<Data, Decoder> {
-//     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-//     where
-//         D: serde::Deserializer<'de>,
-//     {
-//         let prim = Value::deserialize(deserializer)?;
-//         let mut decoder = Decoder::default();
-//         let data = decoder
-//             .decode(&prim)
-//             .map_err(|_| serde::de::Error::custom("Unable to decode"))?;
-//         Ok(Decodable {
-//             data,
-//             decoder: std::marker::PhantomData::default(),
-//         })
-//     }
-// }
+impl<'de, D, U: Unmarshal<D>> Deserialize<'de> for Unmarshaled<D, U> {
+    fn deserialize<De>(deserializer: De) -> Result<Self, De::Error>
+    where
+        De: serde::Deserializer<'de>,
+    {
+        let v = Value::deserialize(deserializer)?;
+        let mut u = U::default();
+        let data = u
+            .unmarshal(v)
+            .map_err(|_| serde::de::Error::custom("Unable to decode"))?;
+        Ok(Unmarshaled {
+            data,
+            u: std::marker::PhantomData::default(),
+        })
+    }
+}
