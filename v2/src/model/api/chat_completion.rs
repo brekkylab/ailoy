6427e19a1c0e4c2f9120a1295f01257da8b8eb05
grf53 <grf0503@gmail.com>
2025-09-29 01:04:37 +0900

    fix: Chat Completion API only for XAI grok

--- v2/src/model/api/chat_completion.rs
+++ v2/src/model/api/chat_completion.rs
@@ -1,20 +1,28 @@
 use crate::{
-    model::sse::ServerSentEvent,
-    value::{ChatCompletionMarshal, LMConfig, Marshaled, Message, MessageDelta, ToolDesc},
+    model::{APIModel, sse::ServerSentEvent},
+    value::{
+        ChatCompletionMarshal, ChatCompletionUnmarshal, FinishReason, LMConfig, Marshaled, Message,
+        MessageDelta, MessageOutput, Part, Role, ToolDesc, Unmarshaled,
+    },
 };
 
 pub fn make_request(
-    api_key: &str,
+    api_model: &APIModel,
     msgs: Vec<Message>,
     tools: Vec<ToolDesc>,
     config: LMConfig,
 ) -> reqwest::RequestBuilder {
-    let model_name = config.model.unwrap_or_default();
-    let mut body = serde_json::json!({
-        "model": model_name,
-        "messages": Marshaled::<_, ChatCompletionMarshal>::new(&msgs),
-        "stream": true
-    });
+    let mut body = serde_json::json!(Marshaled::<_, ChatCompletionMarshal>::new(&config));
+    let msgs = if let Some(system_message) = config.system_message {
+        let mut new_msgs =
+            vec![Message::new(Role::System).with_contents([Part::text(system_message)])];
+        new_msgs.extend(msgs);
+        new_msgs
+    } else {
+        msgs
+    };
+    body["messages"] = serde_json::json!(Marshaled::<_, ChatCompletionMarshal>::new(&msgs));
+
     if !tools.is_empty() {
         body["tool_choice"] = serde_json::json!("auto");
         body["tools"] = serde_json::json!(
@@ -28,38 +36,56 @@ pub fn make_request(
     reqwest::Client::new()
         .request(
             reqwest::Method::POST,
-            "https://api.openai.com/v1/chat/completions",
+            api_model.endpoint(),
         )
-        .bearer_auth(api_key)
-        .header("Content-Type", "application/json")
+        .bearer_auth(api_model.api_key.clone())
+        .header(reqwest::header::CONTENT_TYPE, "application/json")
         .header(reqwest::header::ACCEPT, "text/event-stream")
         .body(body.to_string())
 }
 
-pub fn handle_event(evt: ServerSentEvent) -> Vec<MessageDelta> {
+pub fn handle_event(evt: ServerSentEvent) -> MessageOutput {
     let Ok(j) = serde_json::from_str::<serde_json::Value>(&evt.data) else {
-        return Vec::new();
+        return MessageOutput::default();
     };
     let Some(choice) = j.pointer("/choices/0/delta") else {
-        return Vec::new();
+        return MessageOutput::default();
     };
-    let Ok(decoded) = serde_json::from_value::<
-        crate::value::Unmarshaled<_, crate::value::ChatCompletionUnmarshal>,
-    >(choice.clone()) else {
-        return Vec::new();
+    let finish_reason = choice
+        .pointer("/finish_reason")
+        .and_then(|v| v.as_str())
+        .map(|reason| match reason {
+            "stop" => FinishReason::Stop,
+            "length" => FinishReason::Length,
+            "tool_calls" => FinishReason::ToolCall,
+            "content_filter" => {
+                FinishReason::Refusal("Model output violated XAI's safety policy.".to_owned())
+            }
+            reason => FinishReason::Refusal(format!("reason: {}", reason)),
+        });
+
+    let delta = match finish_reason {
+        Some(FinishReason::Refusal(_)) => MessageDelta::default(),
+        _ => serde_json::from_value::<Unmarshaled<_, ChatCompletionUnmarshal>>(choice.clone())
+            .ok()
+            .map(|decoded| decoded.get())
+            .unwrap_or_default(),
     };
-    let rv = decoded.get();
-    vec![rv]
+
+    MessageOutput {
+        delta,
+        finish_reason,
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::{
-        model::{LanguageModel as _, sse::SSELanguageModel},
+        model::{APIProvider, LanguageModel as _, sse::SSELanguageModel},
         value::{Delta, LMConfigBuilder},
     };
 
-    const OPENAI_API_KEY: &str = "";
+    const XAI_API_KEY: &str = "";
 
     #[tokio::test]
     async fn infer_simple_chat() {
@@ -68,7 +94,8 @@ mod tests {
         use super::*;
         use crate::value::{Part, Role};
 
-        let mut model = SSELanguageModel::new("gpt-4.1", OPENAI_API_KEY);
+        let model =
+            SSELanguageModel::new(APIModel::new(APIProvider::XAI, "grok-4-0709", XAI_API_KEY));
 
         let msgs = vec![
             Message::new(Role::System).with_contents([Part::text("You are a helpful assistant.")]),
@@ -93,11 +120,13 @@ mod tests {
 
         use super::*;
         use crate::{
+            model::{APIModel, APIProvider},
             to_value,
-            value::{Part, Role, ToolDescBuilder},
+            value::{MessageDelta, Part, Role, ToolDescBuilder},
         };
 
-        let mut model = SSELanguageModel::new("gpt-4.1", OPENAI_API_KEY);
+        let model =
+            SSELanguageModel::new(APIModel::new(APIProvider::XAI, "grok-4-0709", XAI_API_KEY));
         let tools = vec![
             ToolDescBuilder::new("temperature")
                 .description("Get current temperature")
