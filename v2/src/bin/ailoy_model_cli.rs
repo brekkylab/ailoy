f7676f7cdf9cb56fc49190e4c00b944b52faf2b8
khj809 <onsealeatang@gmail.com>
2025-10-02 17:27:14 +0900

    add download command

--- v2/src/bin/ailoy_model_cli.rs
+++ v2/src/bin/ailoy_model_cli.rs
@@ -1,9 +1,13 @@
 use std::path::PathBuf;
 
-use ailoy::cache::{Manifest, ManifestDirectory};
-use anyhow::anyhow;
+use ailoy::{
+    cache::{Cache, Manifest, ManifestDirectory},
+    model::get_accelerator,
+};
 use aws_config::meta::region::ProvideRegion;
 use clap::{Parser, Subcommand};
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use tokio::io::AsyncWriteExt;
 
 #[derive(Parser, Debug)]
 #[command(version, about = "Ailoy Model Manager CLI", long_about = None)]
@@ -16,16 +20,44 @@ struct Cli {
 enum Commands {
     Upload {
         model_path: PathBuf,
+
+        #[arg(short, long, default_value = "default")]
         aws_profile_name: String,
 
         #[arg(short, long, default_value = None)]
         aws_endpoint_url: Option<String>,
+
+        #[arg(short, long, default_value = "ailoy-cache")]
+        s3_bucket_name: String,
     },
     Download {
         model_name: String,
-        arch: String,
-        os: String,
-        device: String,
+
+        #[arg(
+            short,
+            long,
+            help = "Target platform triple (e.g. aarch64-apple-darwin). If not provided, it's inferred based on the current environment."
+        )]
+        platform: Option<String>,
+
+        #[arg(
+            short,
+            long,
+            help = "Target device (e.g. metal). If not provided, it's inferred based on the current environment."
+        )]
+        device: Option<String>,
+
+        #[arg(short, long)]
+        download_path: Option<PathBuf>,
+
+        #[arg(short, long, default_value = "default")]
+        aws_profile_name: String,
+
+        #[arg(short, long)]
+        aws_endpoint_url: Option<String>,
+
+        #[arg(short, long, default_value = "ailoy-cache")]
+        s3_bucket_name: String,
     },
 }
 
@@ -38,50 +70,81 @@ async fn main() -> anyhow::Result<()> {
             model_path,
             aws_profile_name,
             aws_endpoint_url,
+            s3_bucket_name,
         } => {
             if !model_path.is_dir() {
-                log::error!(
+                eprintln!(
                     "Model path does not exist: {}",
                     model_path.to_string_lossy()
                 );
                 std::process::exit(1);
             }
 
-            let bucket_name = "ailoy-cache";
             let model_name = model_path.file_name().unwrap().to_str().unwrap();
+
             let client = get_s3_client(aws_profile_name, aws_endpoint_url).await?;
+            let target_entries = std::fs::read_dir(model_path)
+                .unwrap()
+                .into_iter()
+                .map(|entry| entry.unwrap().path())
+                .filter(|path| {
+                    let filename = path.file_name().unwrap().to_str().unwrap();
+                    path.is_file() && filename != "_manifest.json"
+                })
+                .collect::<Vec<_>>();
+            let mut manifests = ManifestDirectory::new();
+
+            let mp = MultiProgress::new();
+            let spinner_style = ProgressStyle::default_spinner()
+                .template("{spinner:.green} {msg}")
+                .unwrap()
+                .tick_strings(&["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]);
 
             println!("Uploading model files from {:?}", model_path);
+            for path in target_entries {
+                let filename = path.file_name().unwrap().to_string_lossy().to_string();
 
-            let mut manifests = ManifestDirectory::new();
-            for entry in std::fs::read_dir(model_path).unwrap() {
-                let entry = entry.unwrap();
-                let path = entry.path();
+                let pb = mp.add(ProgressBar::new_spinner());
+                pb.set_style(spinner_style.clone());
+                pb.set_message(filename.clone());
+                pb.enable_steady_tick(std::time::Duration::from_millis(80));
 
-                if path.is_file() {
-                    let filename = path.file_name().unwrap().to_str().unwrap();
-                    if filename == "_manifest.json" {
-                        continue;
+                let content = std::fs::read(path.clone())?;
+                let s3_body: aws_sdk_s3::primitives::ByteStream = content.into();
+                let manifest = Manifest::from_u8(s3_body.bytes().unwrap());
+                manifests.insert_file(
+                    path.file_name().unwrap().to_str().unwrap().to_owned(),
+                    manifest.clone(),
+                );
+
+                match client
+                    .put_object()
+                    .bucket(s3_bucket_name.clone())
+                    .key(format!("{}/{}", model_name, manifest.sha1()))
+                    .body(s3_body)
+                    .send()
+                    .await
+                {
+                    Ok(_) => {
+                        pb.set_style(
+                            ProgressStyle::default_spinner()
+                                .template("{prefix:.green} {msg}")
+                                .unwrap(),
+                        );
+                        pb.set_prefix("✓");
+                        pb.finish();
                     }
-
-                    println!(" - {:?}", filename);
-
-                    let content = std::fs::read(path.clone())?;
-                    let s3_body: aws_sdk_s3::primitives::ByteStream = content.into();
-                    let manifest = Manifest::from_u8(s3_body.bytes().unwrap());
-                    manifests.insert_file(
-                        path.file_name().unwrap().to_str().unwrap().to_owned(),
-                        manifest.clone(),
-                    );
-
-                    client
-                        .put_object()
-                        .bucket(bucket_name)
-                        .key(format!("{}/{}", model_name, manifest.sha1()))
-                        .body(s3_body)
-                        .send()
-                        .await
-                        .map_err(|e| anyhow!(e))?;
+                    Err(e) => {
+                        pb.set_style(
+                            ProgressStyle::default_spinner()
+                                .template("{prefix:.red} {msg}")
+                                .unwrap(),
+                        );
+                        pb.set_prefix("x");
+                        pb.finish();
+                        eprintln!("Failed to upload {}: {}", filename, e);
+                        std::process::exit(1);
+                    }
                 }
             }
 
@@ -89,21 +152,128 @@ async fn main() -> anyhow::Result<()> {
             let body: aws_sdk_s3::primitives::ByteStream = manifests_json.into();
             client
                 .put_object()
-                .bucket(bucket_name)
+                .bucket(s3_bucket_name)
                 .key(format!("{}/_manifest.json", model_name))
                 .body(body)
                 .content_type("application/json")
                 .send()
                 .await
-                .map_err(|e| anyhow!(e))?;
+                .map_err(|e| {
+                    eprintln!("Failed to upload _manifest.json: {}", e);
+                    std::process::exit(1);
+                })?;
+
+            println!("🎉 Upload complete!")
         }
         Commands::Download {
             model_name,
-            arch,
-            os,
+            platform,
             device,
+            download_path,
+            aws_profile_name,
+            aws_endpoint_url,
+            s3_bucket_name,
         } => {
-            println!("Download!");
+            let model_name = model_name.clone().replace("/", "--");
+            let platform = platform
+                .clone()
+                .unwrap_or(env!("BUILD_TARGET_TRIPLE").to_string());
+            let device = device.clone().unwrap_or(get_accelerator().to_string());
+            let download_path = download_path
+                .clone()
+                .unwrap_or(Cache::new().root().to_path_buf());
+
+            println!("Downloading {}", model_name);
+            println!("- Platform: {}", platform);
+            println!("- Device: {}", device);
+            println!("- Download path: {:?}", download_path);
+
+            let client = get_s3_client(aws_profile_name, aws_endpoint_url).await?;
+
+            let model_dirs = vec![
+                model_name.clone(),
+                format!("{}--{}--{}", model_name, platform, device),
+            ];
+
+            let mp = MultiProgress::new();
+            let spinner_style = ProgressStyle::default_spinner()
+                .template("{spinner:.green} {msg}")
+                .unwrap()
+                .tick_strings(&["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]);
+
+            for model_dir in model_dirs.into_iter() {
+                let dir_resp = client
+                    .list_objects_v2()
+                    .bucket(s3_bucket_name)
+                    .prefix(format!("{}/", model_dir))
+                    .max_keys(1)
+                    .send()
+                    .await?;
+                let objects = dir_resp.contents();
+                if objects.is_empty() {
+                    eprintln!("Model directory '{}' not found in S3 bucket.", model_dir);
+                    std::process::exit(1);
+                }
+
+                let manifest_obj = client
+                    .get_object()
+                    .bucket(s3_bucket_name)
+                    .key(format!("{}/_manifest.json", model_dir))
+                    .send()
+                    .await
+                    .map_err(|e| {
+                        eprintln!(
+                            "Failed to read _manifest.json for model directory '{}': {}",
+                            model_dir,
+                            e.to_string()
+                        );
+                        std::process::exit(1);
+                    })?;
+                let manifest_data = manifest_obj.body.collect().await?.into_bytes();
+                let manifest_dir: ManifestDirectory =
+                    serde_json::from_slice(manifest_data.iter().as_slice())?;
+
+                let download_dir = format!("{}/{}", download_path.to_str().unwrap(), model_dir);
+                std::fs::create_dir_all(&download_dir).map_err(|e| {
+                    eprintln!(
+                        "Failed to create directory {:?}: {}",
+                        download_dir,
+                        e.to_string()
+                    );
+                    std::process::exit(1);
+                })?;
+
+                for (filename, manifest) in manifest_dir.files.iter() {
+                    let remote_key = format!("{}/{}", model_dir, manifest.sha1());
+                    let local_key = format!("{}/{}", model_dir, filename);
+
+                    let pb = mp.add(ProgressBar::new_spinner());
+                    pb.set_style(spinner_style.clone());
+                    pb.set_message(local_key.clone());
+                    pb.enable_steady_tick(std::time::Duration::from_millis(80));
+
+                    let file_obj = client
+                        .get_object()
+                        .bucket(s3_bucket_name)
+                        .key(remote_key)
+                        .send()
+                        .await?;
+                    let file_data = file_obj.body.collect().await?.into_bytes();
+                    let mut file =
+                        tokio::fs::File::create(format!("{}/{}", download_dir, filename)).await?;
+                    file.write_all(&file_data).await?;
+
+                    pb.set_style(
+                        ProgressStyle::default_spinner()
+                            .template("{prefix:.green} {msg}")
+                            .unwrap(),
+                    );
+                    pb.set_prefix("✓");
+                    pb.finish();
+                }
+            }
+
+            println!("🎉 Download complete!");
         }
     }
 
