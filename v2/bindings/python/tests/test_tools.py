c144c79cf086a10a31ac850b53b657eebf186d7d
grf53 <grf0503@gmail.com>
2025-10-20 13:31:59 +0900

    test: improve `test_tools.py`

--- v2/bindings/python/tests/test_tools.py
+++ v2/bindings/python/tests/test_tools.py
@@ -30,8 +30,8 @@ async def test_python_async_function_tool():
         Returns:
             int: The temperature
         """
-        await asyncio.sleep(1.0)
-        return 36
+        await asyncio.sleep(0.2)
+        return 35 if unit == "Celsius" else 95
 
     def tool_temperature_sync(
         location: str, unit: Literal["Celsius", "Fahrenheit"] = "Celsius"
@@ -44,64 +44,56 @@ async def test_python_async_function_tool():
         Returns:
             int: The temperature
         """
-        print(f"{location=}")
-        print(f"{unit=}")
-        return 36
-
-    desc = ai.ToolDesc(
-        "temperature",
-        "Get temperature of the provided location",
-        {
-            "type": "object",
-            "properties": {
-                "location": {
-                    "type": "string",
-                    "description": "The city name",
-                },
-                "unit": {
-                    "type": "string",
-                    "description": "temperature unit",
-                    "enum": ["Celsius", "Fahrenheit"],
-                },
-            },
-            "required": ["location", "unit"],
-        },
-    )
+        return 35 if unit == "Celsius" else 95
 
     # with async function
-    tool = ai.Tool.new_py_function(desc, tool_temperature)
-
-    # __call__() (async)
-    print(f'{await tool(location="Seoul")=}')
-    print(f'{await tool(location="Seoul", unit="Fahrenheit")=}')
-
-    # call() (async)
-    print(f'{await tool.call(location="Seoul")=}')
-    print(f'{await tool.call(location="Seoul", unit="Fahrenheit")=}')
-
-    # with sync function
-    tool_sync = ai.Tool.new_py_function(desc, tool_temperature_sync)
+    tool = ai.Tool.new_py_function(tool_temperature)
 
     # async calls with __call__()
-    print(f'{await tool_sync(location="Seoul")=}')
-    print(f'{await tool_sync(location="Seoul", unit="Fahrenheit")=}')
+    assert await tool(location="Seoul") == 35
+    assert await tool(location="Seoul", unit="Fahrenheit") == 95
 
     # async calls with call()
-    print(f'{await tool_sync.call(location="Seoul")=}')
-    print(f'{await tool_sync.call(location="Seoul", unit="Fahrenheit")=}')
+    assert await tool.call(**{"location": "Seoul"}) == 35
+    assert await tool.call(**{"location": "Seoul", "unit": "Fahrenheit"}) == 95
+
+    # with sync function
+    tool_sync = ai.Tool.new_py_function(tool_temperature_sync)
+
+    # async calls with __call__()
+    assert await tool_sync(location="Seoul") == 35
+    assert await tool_sync(location="Seoul", unit="Fahrenheit") == 95
+
+    # async calls with call()
+    assert await tool_sync.call(**{"location": "Seoul"}) == 35
+    assert await tool_sync.call(**{"location": "Seoul", "unit": "Fahrenheit"}) == 95
 
     # sync calls with call_sync()
-    print(f'{tool_sync.call_sync(location="Seoul")=}')
-    print(f'{tool_sync.call_sync(location="Seoul", unit="Fahrenheit")=}')
+    assert tool_sync.call_sync(location="Seoul") == 35
+    assert tool_sync.call_sync(location="Seoul", unit="Fahrenheit") == 95
 
 
 async def test_mcp_tools():
+    import json
+    from datetime import datetime, timedelta
+    from zoneinfo import ZoneInfo
+
     mcp_client = await ai.MCPClient.from_stdio("uvx", ["mcp-server-time"])
 
     # call tools() to get list of MCP tools
     current_time_tool = mcp_client.tools[0]
-    print(await current_time_tool(timezone="Asia/Seoul"))
+    tool_result = json.loads(await current_time_tool(timezone="Asia/Seoul"))
+    assert tool_result["timezone"] == "Asia/Seoul"
+    assert abs(
+        datetime.fromisoformat(tool_result["datetime"])
+        - datetime.now(tz=ZoneInfo("Asia/Seoul"))
+    ) < timedelta(seconds=5)
 
     # call get_tool(name) to get the MCP tool with the given name
     current_time_tool = mcp_client.get_tool("get_current_time")
-    print(current_time_tool.call_sync(timezone="Asia/Seoul"))
+    tool_result = json.loads(current_time_tool.call_sync(timezone="Asia/Seoul"))
+    assert tool_result["timezone"] == "Asia/Seoul"
+    assert abs(
+        datetime.fromisoformat(tool_result["datetime"])
+        - datetime.now(tz=ZoneInfo("Asia/Seoul"))
+    ) < timedelta(seconds=5)
