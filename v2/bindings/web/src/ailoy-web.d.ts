559e6ef7b21fb46b9e6e985d501f37c391db68c2
grf53 <grf0503@gmail.com>
2025-10-29 21:51:29 +0900

    chore: fix weird docstring

--- v2/bindings/web/src/ailoy-web.d.ts
+++ v2/bindings/web/src/ailoy-web.d.ts
@@ -3,6 +3,7 @@
  * It manages the entire reasoning and action loop, coordinating how each subsystem contributes to the final response.
  *
  * In essence, the Agent:
+ *
  * - Understands user input
  * - Interprets structured responses from the language model (such as tool calls)
  * - Executes tools as needed
@@ -394,7 +395,7 @@ export type PartDelta =
  *     name: \"translate\".into(),
  *     arguments: r#\"{\"text\":\"hi\"}\"#.into(),
  * };
- * `
+ * ```
  */
 export type PartDeltaFunction =
   | { type: "verbatim"; text: string }
