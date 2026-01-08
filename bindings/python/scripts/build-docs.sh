7910b4afc14542e49bf75c0ce9c2916e7bf758ba
khj809 <onsealeatang@gmail.com>
2026-01-08 18:43:18 +0900

    add output_dir argument

--- bindings/python/scripts/build-docs.sh
+++ bindings/python/scripts/build-docs.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Optional argument: output directory for the generated site
+# Usage: ./build-docs.sh [output_dir]
+# If not specified, mkdocs will use the default location (./site)
+
+SITE_DIR="${1:-}"
+MKDOCS_BUILD_ARGS=""
+
+if [ -n "$SITE_DIR" ]; then
+  MKDOCS_BUILD_ARGS="$MKDOCS_BUILD_ARGS --site-dir $SITE_DIR"
+fi
+
 uv pip compile --group docs --quiet -o /tmp/requirements-docs.txt
-uvx --with-requirements /tmp/requirements-docs.txt mkdocs build
+uvx --with-requirements /tmp/requirements-docs.txt mkdocs build $MKDOCS_BUILD_ARGS
 rm -f /tmp/requirements-docs.txt
