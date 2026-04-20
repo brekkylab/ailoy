29ccbfd8351d53dfff77acbe440dae52832a3284
jhlee525 <bmrcreative90@gmail.com>
2026-04-20 18:50:13 +0900

    Update src/lib.rs
    Co-authored-by: Haejoon Kim <onsealeatang@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ extern crate self as ailoy;
 pub mod agent;
 pub mod datatype;
 pub mod lang_model;
-mod lang_model_impl;
+pub(crate) mod lang_model_impl;
 pub(crate) mod macros;
 pub mod message;
 pub mod shell;
